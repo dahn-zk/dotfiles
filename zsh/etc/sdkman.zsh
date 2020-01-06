3395644bafb53826c8c23ae456b34da54682a11d
Dan Oak <danylo.dubinin@gmail.com>
2020-01-06 21:01:25 +0200

    fix

--- zsh/etc/sdkman.zsh
+++ zsh/etc/sdkman.zsh
@@ -5,7 +5,7 @@ function () {
   if [[ -d $SDKMAN_DIR ]]; then
     export SDKMAN_DIR
     local JAVA_HOME=$SDKMAN_DIR/candidates/java/current
-    if [[ -d JAVA_HOME ]]; then export JAVA_HOME; fi
+    if [[ -d $JAVA_HOME ]]; then export JAVA_HOME; fi
     local s=$SDKMAN_DIR/bin/sdkman-init.sh
     if [[ -s $sdkman_init_script ]]; then source $sdkman_init_script; fi
   fi
