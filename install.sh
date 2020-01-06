3a2c42747d84481730118cd8f1a1777fbd289834
Dan Oak <danylo.dubinin@gmail.com>
2020-01-06 21:02:19 +0200

    gradle

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ chmod +x ~/bin/cht.sh
 curl -o ./nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Gradle
+# ~/.gradle/gradle.properties: org.gradle.java.home=/Users/danylo.dubinin/.sdkman/candidates/java/current
