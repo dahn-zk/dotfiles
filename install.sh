d0c397e2dec810f42f34a12e0620f8be5f2fc58d
Dan Oak <danylo.dubinin@gmail.com>
2020-01-09 23:26:35 +0200

    back n forth

--- install.sh
+++ install.sh
@@ -6,5 +6,38 @@ chmod +x ~/bin/cht.sh
 curl -o ./nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+print "GNU getopt"
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+# https://gist.github.com/bobpaul/ecd74cdf7681516703f20726431eaceb
+brew install gnu-getopt
+
+print "SDKMAN! - https://sdkman.io"
+curl -s "https://get.sdkman.io" | bash
+source $HOME/.sdkman/bin/sdkman-init.sh
+sdk version
+
 # Gradle
 # ~/.gradle/gradle.properties: org.gradle.java.home=/Users/danylo.dubinin/.sdkman/candidates/java/current
+
+# http://www.figlet.org/
+brew install figlet
+
+# https://the.exa.website/
+brew install exa
+
+# The Silver Searcher
+# https://github.com/ggreer/the_silver_searcher
+brew install the_silver_searcher
+
+# csvkit
+# https://csvkit.readthedocs.io
+pipenv install csvkit
+
+# PostgreSQL
+brew install postgresql
+brew services start postgresql
+initdb /usr/local/var/postgres
+pg_ctl -D /usr/local/var/postgres status
+
+# csvkit -> postgresql
+pipenv install psycopg2-binary
--- zsh/install.zsh
+++ zsh/install.zsh
@@ -17,36 +17,3 @@ rm -f ~/.zcompdump-* .zcompdump-*
 # https://github.com/zsh-users/zsh-syntax-highlighting
 brew install zsh-syntax-highlighting
 
-print "GNU getopt"
-# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
-# https://gist.github.com/bobpaul/ecd74cdf7681516703f20726431eaceb
-brew install gnu-getopt
-
-print "SDKMAN! - https://sdkman.io"
-curl -s "https://get.sdkman.io" | bash
-source $HOME/.sdkman/bin/sdkman-init.sh
-sdk version
-
-# http://www.figlet.org/
-brew install figlet
-
-# https://the.exa.website/
-brew install exa
-
-# The Silver Searcher
-# https://github.com/ggreer/the_silver_searcher
-brew install the_silver_searcher
-
-# csvkit
-# https://csvkit.readthedocs.io
-pipenv install csvkit
-
-# PostgreSQL
-brew install postgresql
-brew services start postgresql
-initdb /usr/local/var/postgres
-pg_ctl -D /usr/local/var/postgres status
-
-# csvkit -> postgresql
-pipenv install psycopg2-binary
-
