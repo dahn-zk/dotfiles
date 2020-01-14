f833bd91f49e1a0dead39c142d6155e3ede6a12d
Dan Oak <danylo.dubinin@gmail.com>
2020-01-14 21:26:25 +0200

    fix paht

--- zsh/etc/gnu-utils.zsh
+++ zsh/etc/gnu-utils.zsh
@@ -1,29 +1,34 @@
 #!/usr/bin/env zsh
 
-paht=(/usr/local/opt/gnu-tar/libexec/gnubin $path)
+# tar
+path=(/usr/local/opt/gnu-tar/libexec/gnubin $path)
 manpath+=/usr/local/opt/gnu-tar/libexec/gnuman
 
-paht=(/usr/local/opt/grep/libexec/gnubin $path)
+# grep
+path=(/usr/local/opt/grep/libexec/gnubin $path)
 manpath+=/usr/local/opt/grep/libexec/gnuman
 
-paht=(/usr/local/opt/gnu-getopt/bin $path)
-manpath+=/usr/local/opt/gnu-getopt/libexec/gnuman
-
-paht=(/usr/local/opt/gnu-indent/libexec/gnubin $path)
+# indent
+path=(/usr/local/opt/gnu-indent/libexec/gnubin $path)
 manpath+=/usr/local/opt/gnu-indent/libexec/gnuman
 
-paht=(/usr/local/opt/findutils/libexec/gnubin $path)
+# findutils
+path=(/usr/local/opt/findutils/libexec/gnubin $path)
 manpath+=/usr/local/opt/findutils/libexec/gnuman
 
-paht=(/usr/local/opt/coreutils/libexec/gnubin $path)
+# coreutils
+path=(/usr/local/opt/coreutils/libexec/gnubin $path)
 manpath+=/usr/local/opt/coreutils/libexec/gnuman
 
-paht=(/usr/local/opt/gnu-sed/libexec/gnubin $path)
+# sed
+path=(/usr/local/opt/gnu-sed/libexec/gnubin $path)
 manpath+=/usr/local/opt/gnu-sed/libexec/gnuman
 
+# getopt
 path=(/usr/local/opt/gnu-getopt/bin $path)
 manpath+=/usr/local/opt/gnu-getopt/share/man
 
+# m4
 path=(/usr/local/opt/m4/bin $path)
 manpath+=/usr/local/opt/m4/share/man
 
