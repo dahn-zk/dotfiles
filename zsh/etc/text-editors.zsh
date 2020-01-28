8af8567b67a166f03fd922286430b265b3a67120
Dan Oak <danylo.dubinin@gmail.com>
2020-01-28 18:35:32 +0200

    vscode

---
+++ zsh/etc/text-editors.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+#if [ -d /Applications/Visual\ Studio\ Code.app/ ]; then
+#  export EDITOR="/Applications/Visual\ Studio\ Code.app/"
+#fi
+
+alias vscode="open -a 'Visual Studio Code'"
