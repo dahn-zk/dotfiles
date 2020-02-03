8b8a153d543a99bcc2e59970141056dfcdba9ccc
Dan Oak <danylo.dubinin@gmail.com>
2020-02-03 12:47:48 +0200

    url codec

--- zsh/etc/url-codec.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-alias urldec='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
-alias urlenc='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
