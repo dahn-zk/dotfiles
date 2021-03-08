2b5c251603c90607da9fb411c7ac2c3ddd9ede9e
Dan Oak <curly-brace-17@protonmail.com>
2021-03-08 12:53:30 +0200

    + \n to prompt for venvs promp mods etc

--- zsh/etc/006--prompt.zsh
+++ zsh/etc/006--prompt.zsh
@@ -3,6 +3,6 @@
 autoload -U colors && colors
 setopt prompt_subst
 # PROMPT='$(printf "\u001b[38;5;0239m%-*s" ${COLUMNS:-$(tput cols)} $PWD\$ | tr " $" "- ")'
-PROMPT='$(printf "\u001b[38;5;0239m%-*s" ${COLUMNS:-$(tput cols)} $?\$ | tr " $" "- ")'
+PROMPT=$'\n''$(printf "\u001b[38;5;0239m%-*s" ${COLUMNS:-$(tput cols)} $?\$ | tr " $" "- ")'
 PROMPT+=$'\n''$(pwd)'
 PROMPT+=$'\n'"%(?:%{$fg_bold[green]%}%\> :%{$fg_bold[red]%}%\> )%{$reset_color%}"
