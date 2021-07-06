528a6a7cb76a920c9fbce876e7b76c8b69c07497
Curly Brace 17 <curly-brace-17@protonmail.com>
2021-07-06 17:58:22 +0300

    fix

--- zsh/etc/004--xdg-env-vars.zsh
+++ zsh/etc/004--xdg-env-vars.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
-if [$(uname -s) == Linux*]; then
-  export XDG_CONFIG_HOME="$HOME/.config"
-  export XDG_CACHE_HOME="$HOME/.cache"
-  export XDG_DATA_HOME="$HOME/.local/share"
-fi
+case $(uname -s) in
+  Linux*)
+    export XDG_CONFIG_HOME="$HOME/.config"
+    export XDG_CACHE_HOME="$HOME/.cache"
+    export XDG_DATA_HOME="$HOME/.local/share"
+    ;;
+esac
