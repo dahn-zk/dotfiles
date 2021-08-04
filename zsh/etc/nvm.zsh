8b2c19639d950b23d843bf8c83192949d11d2dc7
Dan Oak <curly-brace-17@protonmail.com>
2021-08-04 09:38:35 +0300

    load nvm lazily

--- zsh/etc/nvm.zsh
+++ zsh/etc/nvm.zsh
@@ -1,5 +1,30 @@
 # NodeJS version manager 
 # https://github.com/nvm-sh/nvm
 
+# improving slow loading performance: http://broken-by.me/lazy-load-nvm/
+
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm() {
+  unset -f nvm
+  unset -f node
+  unset -f npm
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  nvm $@
+}
+
+node() {
+  unset -f nvm
+  unset -f node
+  unset -f npm
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  node $@
+}
+
+npm() {
+  unset -f nvm
+  unset -f node
+  unset -f npm
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+  npm $@
+}
